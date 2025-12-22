4a90b982288c9b63ed6d9bb542ee8dbd2b5b5df8
sonal kumar mandal <152254912+SONALkumarmandal@users.noreply.github.com>
2025-12-22 18:46:03 +0530

    hover feature

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -51,7 +51,7 @@ const Header = () => {
         ) : (
           <NavLink
             to="/login"
-            className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition"
+            className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-500 transition"
           >
             Sign In
           </NavLink>
